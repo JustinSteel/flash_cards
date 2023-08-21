a79c099eadd4cc452aaa9d501a2a486117496c7e
Justin Lee <justinslee201100@yahoo.com>
2023-08-21 17:25:33 -0600

    Make turn code

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,4 +4,19 @@ class Turn
         @string = string
         @card = card
     end
+    def guess
+        @guess
+    end
+    def correct?
+        if @guess == @string
+        true
+        else false
+        end
+    end
+    def feedback
+        if @guess == @string
+            "Correct!"
+        else "Incorrect"
+        end
+    end
 end
